69992f9216042343277abe01343004483b944125
Luke McCann <developer-lukemccann@outlook.com>
2019-12-06 19:00:40 +0000

    Fixes issue with Grunt Sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,82 +1,47 @@
-// The "wrapper" function
 module.exports = function(grunt) {
-    
-
-    // Load first, before init config
-    // Loads all grunt plugins
-    require('load-grunt-config')(grunt);
-    
-    // Do grunt-related things in here
     grunt.initConfig({
-        // Metadata from the this file
-        pkg: grunt.file.readJSON('package.json'),
 
-        /*
-        Grunt Sass
-        Compile Sass to CSS
-        https://www.npmjs.com/package/grunt-sass
-        */
         sass: {
-            
-            options: {
-                // Reference to Sass source files
-                // for debugging.
-                sourceMap: true
-            },
             dist: {
-                files: {
-                    // Destination : Source    Multiple sources can be compiled into the destination
-                    'css/style.css': 'assets/sass/style.scss'
-                }
+                files: [{
+                    expand: true,
+                    cwd: './assets/sass',
+                    src: ['*.scss'],
+                    dest: './css',
+                    ext: '.css'
+                }]
             }
         },
 
-        /*
-        * Grunt Contrib Watch
-        * Monitor files and execute tasks
-        * https://www.npmjs.com/package/grunt-contrib-watch
-        */
-
-        watch: {
-            sass: {
-                files: [
-                    'assets/sass/*.scss'
-                ],
-                tasks: [
-                    'sass'
-                ]
-            },
-            scripts: {
-                files: [
-                    'assets/js/*.js'
-                ],
-                tasks: [
-                    'uglify'
-                ]
+        cssmin: {
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: './css',
+                    src: ['*.css', '!*.min.css'],
+                    dest: './css',
+                    ext: '.min.css'
+                }]
             }
         },
-        
-        /*
-        Grunt Uglify
-        Minimises JS file
-        https://www.npmjs.com/package/grunt-contrib-uglify
-        */
+
         uglify: {
+            options: {
+                mangle: false
+            },
             my_target: {
                 files: {
-                    'js/script.js' : ['assets/js/script.js']
+                    './scripts/*.min.js' : ['./scripts/*.js']
                 }
             }
         }
+        
     });
-    // Project and task configuration
-    
-    // Loading Grunt plugins and tasks
-    
-    // Custom tasks
 
-    // Default task(s).
-    // Runs 'watch' function when alongside the 'grunt' command
-    grunt.registerTask('default', ['watch']);
-    grunt.loadNpmTasks('grunt-contrib-connect');
+    // load tasks
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+
+   // grunt.registerTask('default', );
 };
